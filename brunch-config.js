58f18c4c619640f5faf4e0e5f4ed478b389571a4
ink16 <rares@jekyo.com>
2021-10-18 17:33:15 +0300

    changed brunch configs

--- brunch-config.js
+++ brunch-config.js
@@ -12,3 +12,15 @@ exports.files = {
 exports.plugins = {
   babel: {presets: ['latest']}
 };
+
+module.exports = {
+  files: {
+  javascripts: {joinTo: 'app.js'},
+  stylesheets: {joinTo: 'app.css'},
+  },
+  
+  server: {
+  hostname: '0.0.0.0',
+  port: 4139
+  }
+  };
